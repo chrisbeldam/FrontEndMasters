db80e69485ff9c8c13c620564018488df932cb32
Chris Beldam <chrisgbeldam@gmail.com>
2018-03-06 15:09:36 +0000

    Comments

--- JS From Fundamentals To Functional JS/Objects/arrays.js
+++ JS From Fundamentals To Functional JS/Objects/arrays.js
@@ -9,6 +9,6 @@ var person = []; // Arrays are objects so .dot notation works the same way as ob
 
 var plea = "wouldShe";
 person.name = "Mrs White";
-person["plea"] = "I would never";
+person["plea"] = "I would never"; // Bracket notation works for all formats, where dot notation is for strings
 person.plea; // You can use dot notation to look up something you made with bracket notation
 console.log(person.plea);
